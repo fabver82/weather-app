87efdc6452ad00710ba1158a653b5530c468f4b7
Fabien V <fabien.vermeiren@gmail.com>
2022-04-15 16:44:54 +0200

    get coordinates and weather datas for next days

--- src/js/apicall.js
+++ src/js/apicall.js
@@ -1,12 +1,32 @@
 import axios from "axios";
 import API_KEY from "../js/config";
-export default async function getWeather(city) {
+async function getCoordinates(city) {
   try {
     const response = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API_KEY}`
+    );
+    return { lat: response.data[0].lat, lon: response.data[0].lon };
+    // return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
+async function getDatas(coordinates) {
+  console.log(coordinates);
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&units=metric&exclude=minutely,hourly,alerts&appid=${API_KEY}`
     );
     return response;
   } catch (error) {
     console.error(error);
   }
+}
+export default async function getWeather(city) {
+  let data;
+  await getCoordinates(city).then(async (lat, lon) => {
+    data = await getDatas(lat, lon);
+    console.log(data);
+  });
+  return data;
 }
